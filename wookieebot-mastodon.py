0f971e546471983ac57a62b7a6f4ccccaa7a1690
Byn Erso <74805043+ferretbreeder@users.noreply.github.com>
2023-07-04 14:50:26 -0400

    Create wookieebot-mastodon.py

---
+++ wookieebot-mastodon.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+from mastodon import Mastodon
+
+#   Set up Mastodon
+mastodon = Mastodon(
+    access_token = 'token.secret',
+    api_base_url = 'https://skyhoppers.club/'
+)
+
+# grab the article
+r = requests.get('https://starwars.fandom.com/wiki/Special:Random', allow_redirects=False)
+
+tootStr = r.headers['Location']
+
+#put that shit out there!
+mastodon.status_post(tootStr)
